2e5294f81cbb94be1a9a7b0b5cfaec7af1b8f243
Ranodeep Banerjee <63450189+ranodeepbanerjee@users.noreply.github.com>
2023-01-20 16:42:59 +0530

    Update attendance.py

--- Projects/Attendance-system/attendance.py
+++ Projects/Attendance-system/attendance.py
@@ -3,29 +3,26 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
-from PIL import ImageGrab
+from openpyxl import Workbook
 
-# new added
-import openpyxl
-
-wb = openpyxl.load_workbook("Attendance.xlsx")
-wb1 = wb["Sheet1"]
-
-nameList = []
-path = "ImagesAttendance"
-# new added
-# path = 'Attendance-system/ImagesAttendance'
+ 
+path = 'ImageAttendance'
 images = []
 classNames = []
 myList = os.listdir(path)
 print(myList)
+
+#create an empty excel workbook and set the active sheet
+wb = Workbook()
+ws = wb.active
+ws.append(["Name","Time"])
+
 for cl in myList:
-    curImg = cv2.imread(f"{path}/{cl}")
+    curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
     print(classNames)
-
-
+ 
 def findEncodings(images):
     encodeList = []
     for img in images:
@@ -33,81 +30,52 @@ def findEncodings(images):
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
+ 
+already_marked = []
+def markAttendance(name):
+    if name in already_marked:
+        return
+    else:
+        now = datetime.now()
+        dtString = now.strftime('%H:%M:%S')
+        ws.append([name,dtString])
 
-
-"""def markAttendance(name):
-    with open('Attendance-system/Attendance.csv','r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-            if name not in nameList:
-                now = datetime.now()
-                dtString = now.strftime('%H:%M:%S')
-                f.writelines(f'n{name},{dtString}')
- """
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-def captureScreen(bbox=(300, 300, 690 + 300, 530 + 300)):
-    capScr = np.array(ImageGrab.grab(bbox))
-    capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-    return capScr
-
-
+        already_marked.append(name)
+ 
+ 
 encodeListKnown = findEncodings(images)
-print("Encoding Complete")
-
+print('Encoding Complete')
+ 
 cap = cv2.VideoCapture(0)
-f = 0  # new added
+ 
 while True:
     success, img = cap.read()
-    # img = captureScreen()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    #img = captureScreen()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-
+ 
     facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
-
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        # print(faceDis)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+ 
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        #print(faceDis)
         matchIndex = np.argmin(faceDis)
-
+ 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            # print(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(
-                img,
-                name,
-                (x1 + 6, y2 - 6),
-                cv2.FONT_HERSHEY_COMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-            )
-            # markAttendance(name)
-        f = 1  # new added
+            #print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendance(name)
+ 
+        cv2.imshow('Webcam',img)
+        cv2.waitKey(1)
+        markAttendance(name)
 
-    cv2.imshow("Webcam", img)
-    if cv2.waitKey(1) & 0xFF == ord("q"):  # newly added
-        break
-
-    # new added
-    if f == 1:
-
-        if name not in nameList:
-            now = datetime.now()
-            dtString = now.strftime("%H:%M:%S")
-            dt = now.strftime("%d:%m:%Y")
-            wb1.cell(column=1, row=wb1.max_row + 1, value=name)
-            wb1.cell(column=2, row=wb1.max_row, value=dtString)
-            wb1.cell(column=3, row=wb1.max_row, value=dt)
-            nameList.append(name)
-
-    wb.save("Attendance.xlsx")
-    # new added
+        #save the workbook
+        wb.save("Attendance.xlsx")
